bf8f60013571dfe506f911d7273732ecf465801f
notut <noratt66@gmail.com>
2025-03-17 13:16:47 +0100

    Fix formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Application from "./modules/app/Application"; // Sjekk om dette er riktig sti
 
-createRoot(document.getElementById("root")).render(<h1>Hello React</h1>);
+const rootElement = document.getElementById("root");
+
+//Hvis root elementet ikke finnes, kommer det en feilmelding
+if (!rootElement) {
+  throw new Error(
+    "Root element not found. Make sure index.html has <div id='root'></div>",
+  );
+}
+
+//Oppretter en react root
+createRoot(rootElement).render(<Application />);
